222176ccbabd4e592121b9ab78793ebd8a2dd0a6
= <=>
2023-07-04 10:54:03 +0200

    removed duplicate import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { TimerCardComponent } from './timer-card/timer-card.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatButtonModule } from '@angular/material/button';
 import { TimePipe } from './pipe/time.pipe';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, TimerCardComponent, TimePipe],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    TimerCardComponent,
+    TimePipe,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
