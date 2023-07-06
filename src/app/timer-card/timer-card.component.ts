c44edcbf5c4c49171b68385a7cd6d732a9f62079
= <=>
2023-07-06 15:59:07 +0200

    timer

--- src/app/timer-card/timer-card.component.ts
+++ src/app/timer-card/timer-card.component.ts
@@ -1,22 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
+import {
+  Observable,
+  timer,
+  tap,
+  takeWhile,
+  fromEvent,
+  map,
+  switchMap,
+  NEVER,
+  withLatestFrom,
+  scan,
+  of,
+} from 'rxjs';
 
+export type Status = {
+  active: boolean;
+};
 @Component({
   selector: 'app-timer-card',
   templateUrl: './timer-card.component.html',
   styleUrls: ['./timer-card.component.scss'],
 })
-export class TimerCardComponent {
-  timeInSeconds: number = 585;
+export class TimerCardComponent implements AfterViewInit {
+  @Input() timeInSeconds!: number;
+  @Input() id!: string;
   label: string = 'Label';
-  isPaused: Boolean = false;
-  color: string = 'warn';
+  color: string = 'primary';
+  isPaused: Boolean = true;
+  isCompleted: Boolean = false;
+  progress: number = 100;
+  countdown$!: Observable<number>;
+  private pauseClick$!: Observable<Status>;
+  private tick$ = timer(0, 1000);
+
+  ngAfterViewInit(): void {
+    this.pauseClick$ = fromEvent(
+      document.getElementById(this.id) as HTMLButtonElement,
+      'click'
+    ).pipe(
+      map(() => ({
+        active: !this.isPaused,
+      }))
+    );
+    this.countdown$ = this.timer(this.timeInSeconds);
+  }
 
   pauseTimer() {
     this.isPaused = !this.isPaused;
     this.color = this.isPaused ? 'primary' : 'warn';
-    //functionality will be added with the rxjs streams implementation.
   }
+
   deleteTimer() {
     //functionality will be added with the rxjs streams implementation.
+  }
+
+  reloadTimer() {
+    this.isCompleted = false;
+    this.progress = 100;
+    if (!this.isPaused) this.pauseTimer();
+    this.countdown$ = this.timer(this.timeInSeconds);
+  }
+
+  timer(value: number): Observable<number> {
+    return this.tick$.pipe(
+      map(() => 1),
+      withLatestFrom(this.pauseClick$),
+      switchMap(([seconds, status]) => (!status.active ? NEVER : of(seconds))),
+      scan((acc, val) => acc - val, value),
+      tap((timeLeft) => (this.progress = (timeLeft * 100) / value)),
+      takeWhile(Boolean, true),
+      tap(() => {
+        if ((this.isCompleted = !this.progress)) {
+          this.alarm();
+        }
+      })
+    );
+  }
+
+  private alarm() {
+    new Audio(
+      'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/pause.wav'
+    ).play();
   }
 }
--- src/app/timer-list/timer-list.component.ts
+++ src/app/timer-list/timer-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-timer-list',
   templateUrl: './timer-list.component.html',
   styleUrls: ['./timer-list.component.scss'],
 })
-export class TimerListComponent {}
+export class TimerListComponent {
+  testData = [
+    { id: 'timer-1', time: 10 },
+    { id: 'timer-2', time: 300 },
+    { id: 'timer-3', time: 600 },
+  ];
+}
