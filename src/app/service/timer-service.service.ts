b334910bab0bd27c5ab6224f1b9c386e9faba319
Robert Strzelczyk <123934951+rStrzelczyk98@users.noreply.github.com>
2023-07-12 15:46:49 +0200

    timer update (#17)

--- src/app/service/timer-service.service.ts
+++ src/app/service/timer-service.service.ts
@@ -1,27 +1,132 @@
 import { Injectable } from '@angular/core';
-import { Observable, scan, ReplaySubject } from 'rxjs';
+import {
+  Observable,
+  BehaviorSubject,
+  timer,
+  filter,
+  scan,
+  shareReplay,
+  takeWhile,
+  withLatestFrom,
+  tap,
+  Subject,
+  takeUntil,
+} from 'rxjs';
+import { Status } from '../timer-card/timer-card.component';
 
 export type Timer = {
   label: string | null;
   totalTime: number;
   id: number;
+  timerStream?: Observable<number>;
+  status: BehaviorSubject<Status>;
+  destory: Subject<void>;
+  completed: boolean;
 };
 
 @Injectable({
   providedIn: 'root',
 })
 export class TimerService {
-  private timers$ = new ReplaySubject<Timer>();
+  private timers$ = new BehaviorSubject<Timer[]>([]);
 
-  constructor() {}
+  private tick$: Observable<number> = timer(0, 1000);
 
   getTimers(): Observable<Timer[]> {
-    return this.timers$.pipe(
-      scan((acc: Timer[], val: Timer) => [...acc, val], [])
-    );
+    return this.timers$.asObservable();
   }
 
   addTimer(timer: Timer) {
-    this.timers$.next(timer);
+    const array = this.timers$.value;
+    if (array.every((el) => el.id !== timer.id)) {
+      this.timers$.next([...array, timer]);
+    } else {
+      const index = this.findIndex(timer.id);
+      this.timers$.value[index] = timer;
+    }
+  }
+
+  removeTimer(id: number) {
+    const index = this.findIndex(id);
+    this.timers$.value.splice(index, 1);
+  }
+
+  updateTimerStatus(id: number, value: boolean) {
+    const index = this.findIndex(id);
+    this.timers$.value[index].status.next({ active: value });
+  }
+
+  getTimerStatus(id: number) {
+    const index = this.findIndex(id);
+    return this.timers$.value[index].status.asObservable();
+  }
+
+  resetTimerStream(id: number) {
+    const timer = this.getTimerById(id);
+    const index = this.findIndex(id);
+    this.timers$.value[index].destory.next();
+    this.timers$.value[index].destory.complete();
+    this.timers$.value[index].status.complete();
+    this.addTimer({
+      ...timer,
+      completed: false,
+      timerStream: undefined,
+      destory: new Subject<void>(),
+      status: new BehaviorSubject<Status>({ active: false }),
+    });
+  }
+
+  setTimerStream(id: number, stream$: Observable<number>) {
+    const index = this.findIndex(id);
+    this.timers$.value[index].timerStream = stream$;
+  }
+
+  getTimerStream(id: number) {
+    const index = this.findIndex(id);
+    return this.timers$.value[index].timerStream as Observable<number>;
+  }
+
+  setCompletedStatus(id: number, value: boolean) {
+    const index = this.findIndex(id);
+    this.timers$.value[index].completed = value;
+  }
+
+  getCompletedStatus(id: number) {
+    const index = this.findIndex(id);
+    return this.timers$.value[index].completed;
+  }
+
+  timerStream(id: number): Observable<number> {
+    const timer = this.getTimerById(id);
+    return this.tick$.pipe(
+      takeUntil(timer.destory),
+      withLatestFrom(timer.status),
+      tap(console.log),
+      filter(([_, status]) => status.active),
+      scan((acc, _) => acc - 1, timer.totalTime),
+      takeWhile(Boolean, true),
+      tap((time) => {
+        if (!time) {
+          this.setCompletedStatus(id, true);
+          this.alarm();
+        }
+      }),
+      shareReplay(1)
+    );
+  }
+
+  private findIndex(id: number) {
+    return this.timers$.value.findIndex((timer) => timer.id === id);
+  }
+
+  private getTimerById(id: number): Timer {
+    const index = this.findIndex(id);
+    return this.timers$.value[index];
+  }
+
+  private alarm() {
+    new Audio(
+      'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/pause.wav'
+    ).play();
   }
 }
--- src/app/timer-card/timer-card.component.ts
+++ src/app/timer-card/timer-card.component.ts
@@ -1,15 +1,6 @@
-import { Component, Input, AfterViewInit } from '@angular/core';
-import {
-  Observable,
-  timer,
-  tap,
-  takeWhile,
-  fromEvent,
-  map,
-  withLatestFrom,
-  scan,
-  filter,
-} from 'rxjs';
+import { Component, Input, AfterViewInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription, map, tap } from 'rxjs';
+import { TimerService } from '../service/timer-service.service';
 
 export type Status = {
   active: boolean;
@@ -19,70 +10,78 @@ export type Status = {
   templateUrl: './timer-card.component.html',
   styleUrls: ['./timer-card.component.scss'],
 })
-export class TimerCardComponent implements AfterViewInit {
+export class TimerCardComponent implements AfterViewInit, OnDestroy {
   @Input() timeInSeconds!: number;
   @Input() id!: number;
   @Input() label!: string;
   color: string = 'primary';
-  isPaused: Boolean = true;
-  isCompleted: Boolean = false;
+  isPaused!: boolean;
+  isCompleted: boolean = false;
   countdown$!: Observable<number>;
   progress$!: Observable<number>;
-  private pauseClick$!: Observable<Status>;
-  private tick$ = timer(0, 1000);
+  private sub!: Subscription;
+
+  constructor(private ts: TimerService) {}
 
   ngAfterViewInit(): void {
-    this.pauseClick$ = fromEvent(
-      document.getElementById('' + this.id) as HTMLButtonElement,
-      'click'
-    ).pipe(
-      map(() => ({
-        active: !this.isPaused,
-      }))
-    );
-    this.countdown$ = this.timer(this.timeInSeconds);
-    this.progress$ = this.progressBarValue(this.timeInSeconds);
+    this.setStreams();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
   pauseTimer() {
     this.isPaused = !this.isPaused;
-    this.color = this.isPaused ? 'primary' : 'warn';
+    this.ts.updateTimerStatus(this.id, this.isPaused);
+    this.toggleButtonColor();
   }
 
   deleteTimer() {
-    //functionality will be added with the rxjs streams implementation.
+    this.ts.removeTimer(this.id);
   }
 
   reloadTimer() {
-    this.isCompleted = false;
     if (!this.isPaused) this.pauseTimer();
-    this.countdown$ = this.timer(this.timeInSeconds);
-    this.progress$ = this.progressBarValue(this.timeInSeconds);
-  }
-
-  private timer(value: number): Observable<number> {
-    return this.tick$.pipe(
-      withLatestFrom(this.pauseClick$),
-      filter(([_, status]) => status.active),
-      scan((acc, _) => acc - 1, value),
-      takeWhile(Boolean, true)
-    );
+    this.ts.updateTimerStatus(this.id, false);
+    this.resetStreams();
   }
 
   private progressBarValue(value: number) {
     return this.countdown$.pipe(
       map((time) => (time * 100) / value),
-      tap((progress) => {
-        if ((this.isCompleted = !progress)) {
-          this.alarm();
-        }
+      tap((time) => {
+        if (!time) this.timerProgressStatus();
       })
     );
   }
 
-  private alarm() {
-    new Audio(
-      'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/pause.wav'
-    ).play();
+  private setStreams() {
+    setTimeout(() => {
+      if (!this.ts.getTimerStream(this.id)) {
+        this.ts.setTimerStream(this.id, this.ts.timerStream(this.id));
+      }
+      this.timerProgressStatus();
+      this.sub = this.ts
+        .getTimerStatus(this.id)
+        .subscribe(({ active }) => (this.isPaused = active));
+      this.toggleButtonColor();
+      this.countdown$ = this.ts.getTimerStream(this.id);
+      this.progress$ = this.progressBarValue(this.timeInSeconds);
+    });
+  }
+
+  private resetStreams() {
+    setTimeout(() => {
+      this.ts.resetTimerStream(this.id);
+    });
+  }
+
+  private timerProgressStatus() {
+    this.isCompleted = this.ts.getCompletedStatus(this.id);
+  }
+
+  private toggleButtonColor() {
+    this.color = !this.isPaused ? 'primary' : 'warn';
   }
 }
