39b13b6dbf4ee36d3467c9146d352de70ad7968f
= <=>
2023-07-12 12:38:47 +0200

    navigation update

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent {
-  selected: string = 'list';
+export class NavigationComponent implements DoCheck {
+  selected: string = '/';
+
+  constructor(private router: Router) {}
+
+  ngDoCheck(): void {
+    this.selected = this.router.url;
+  }
 
   onClick(tab: string) {
-    this.selected = tab === 'list' ? 'list' : 'add-timer';
+    this.selected = tab ? tab : 'add-timer';
   }
 }
